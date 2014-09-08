a587a932eccff9109117c4dd1d34c9b943d12492
Mitch Lake <mitchell_lake@hotmail.com>
2014-09-07 21:47:49 -0500

    Added translator for nullable enum

---
+++ src/SpecAid/Translations/NullableEnum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using SpecAid.Base;
+using SpecAid.Extentions;
+
+namespace SpecAid.Translations
+{
+    public class NullableEnumTranslation : ITranslation
+    {
+        #region Properties
+
+        public int considerOrder
+        {
+            get
+            {
+                return TranslationOrder.NullableGeneric.ToInt32();
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public object Do(PropertyInfo info, string tableValue)
+        {
+            if (string.IsNullOrWhiteSpace(tableValue))
+                return null;
+
+            var innerType = Nullable.GetUnderlyingType(info.PropertyType);
+
+            return Enum.Parse(innerType, tableValue, true);
+        }
+
+        public bool UseWhen(PropertyInfo info, string tableValue)
+        {
+            var innerType = Nullable.GetUnderlyingType(info.PropertyType);
+
+            if (innerType == null)
+                return false;
+
+            return innerType.IsEnum;
+        }
+
+        #endregion
+    }
+}
